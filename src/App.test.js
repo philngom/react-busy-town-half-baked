5d9a8ae4b6e64690f833b4e3223ee5c7fd024355
Philippe Ngom <philngom@gmail.com>
2022-01-26 17:39:30 -0800

    fixed test again

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/the alien is gobblin up all the electricity!/i);
+  const linkElement = screen.getByText(/The alien is gobblin up all the electricity!/i);
   expect(linkElement).toBeInTheDocument();
 });
